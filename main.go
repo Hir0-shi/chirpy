99dbe9a518b75d90f7aa368388f0454d3d489f14
Hir0-shi <Ahti@users.noreply.fuck-discord.com>
2026-08-08 20:29:51 +0200

    monoliths and decoupling

--- main.go
+++ main.go
@@ -18,15 +18,24 @@ func main() {
 	apiCfg := apiConfig{}
 
 	mux := http.NewServeMux()
-	mux.Handle(
-		"/app/",
+
+	// website endpoint
+	fileServer := http.FileServer(http.Dir(filepathRoot))
+	// handle /app
+	mux.Handle("/app",
 		apiCfg.middlewareMetricsInc(
-			http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))),
+			http.StripPrefix("/app", fileServer),
 		),
 	)
-	mux.HandleFunc("GET /healthz", handlerReadiness)
-	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	// handle /app/
+	mux.Handle("/app/",
+		apiCfg.middlewareMetricsInc(
+			http.StripPrefix("/app", fileServer),
+		),
+	)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -40,7 +49,11 @@ func main() {
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+	// w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+	// build bytes without fmt.Sprintf -> []byte(...) roundtrip
+	var b []byte
+	b = fmt.Appendf(b, "Hits: %d", cfg.fileserverHits.Load())
+	w.Write(b)
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
